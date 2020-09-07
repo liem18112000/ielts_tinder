11a91a25b77bee5f28d32d30de60ad8e5dbac355
Liêm <1859027@itec.hcmus.edu.vn>
2020-09-07 09:39:09 +0700

    Add Newsfeeds Backend

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username', 255);
+            $table->string('name', 255);
+            $table->string('email')->nullable();
             $table->string('password');
             $table->tinyInteger('status')->default(0);
-            $table->string('email')->nullable();
+            $table->timestamp('last_seen')->nullable();
             $table->string('provider')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
--- database/migrations/2020_06_18_140740_create_profiles_table.php
+++ database/migrations/2020_06_18_140740_create_profiles_table.php
@@ -18,11 +18,11 @@ class CreateProfilesTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->text('name');
-            $table->text('hometown');
-            $table->date('dob');
+            $table->text('hometown')->nullable();
+            $table->date('dob')->nullable();
             $table->text('profile_image')->default('https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png');
             $table->float('bandscore')->default(0.0);
-            $table->tinyInteger('achieve_time')->default(1);
+            $table->tinyInteger('achieve_time')->default(0);
             $table->timestamps();
         });
     }
